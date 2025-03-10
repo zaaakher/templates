e90c2cdba1b3c1fcc108e8d7ff33e9a3534ee141
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2025-03-09 23:52:09 -0600

    chore: Update vite-plugin-static-copy to copy blueprints to dedicated directoryblueprints

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
 			targets: [
 				{
 					src: 'blueprints/*',
-					dest: '' // raíz de dist (public root)
+					dest: 'blueprints' // raíz de dist (public root)
 				},
 				{
 					src: 'meta.json',

08b572dcaee86098e0967953ab068c944f4b654d
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2025-03-09 23:36:58 -0600

    feat: Add extensive collection of application blueprints with Docker Compose configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
-// https://vite.dev/config/
+import tailwindcss from '@tailwindcss/vite'
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })

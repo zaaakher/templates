f6ee900087c2d08106f6b2f90d2114cb51533b1a
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2025-03-09 23:45:54 -0600

    chore: Add vite-plugin-static-copy to copy blueprints and meta.json to dist

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
-
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+    viteStaticCopy({
+			targets: [
+				{
+					src: 'blueprints/*',
+					dest: '' // raíz de dist (public root)
+				},
+				{
+					src: 'meta.json',
+					dest: '' // raíz de dist
+				}
+			]
+		})
+
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
